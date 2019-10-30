eecf050567eda514c2eb9d5819537e36c94393d3
javicercasi <jacercasii@gmail.com>
2019-10-30 00:24:29 -0300

    comit local

--- 58004-Cercasi Javier/parcial.py
+++ 58004-Cercasi Javier/parcial.py
@@ -1,3 +1,12 @@
-a= int(input("Ingrese"))
+import sys
+import math
 
-print(a)
+class Sudoku():
+
+    def __init__(self, tabla, tamano):
+        self.tamano = tamano 
+        self.is_playing = True
+        self.matriz = [ [ 0 for __ in range(self.tamano) ] for _ in range(self.tamano) ]
+        self.fijos = [ [ 0 for __ in range(self.tamano) ] for _ in range(self.tamano) ]
+        self.string_converter(tabla)
+        dsaads
